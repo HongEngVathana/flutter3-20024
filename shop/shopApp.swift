8fd0650887b14ede1a8f68837c31b3fc2d6f19de
HongEngVathana <engvathanahong@gmail.com>
2024-11-26 09:42:20 +0700

    Initial Commit

---
+++ shop/shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopApp.swift
+//  shop
+//
+//  Created by MacBook Pro on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
